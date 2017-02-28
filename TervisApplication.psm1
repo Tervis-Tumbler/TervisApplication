3fb42d212426b1742768178f9021b4c3a2cca404
Chris Magnuson <hourglasssand@gmail.com>
2017-02-28 15:52:08 -0500

    Initial commit

---
+++ TervisApplication.psm1
@@ -0,0 +1,41 @@
+﻿$ApplicationDefinition = [PSCustomObject][Ordered]@{
+    Name = "SFTP"
+    VMSizeName = "Small"
+    VMOperatingSystemTemplateName = "CentOS 7"
+    Environmentname = "Infrastructure"
+    Cluster = "hypervcluster5"
+    DHCPScopeID = "10.172.44.0"
+    ComputernameSuffixInAD = "inf-sftp"
+}
+
+Function Get-TervisApplicationDefinition {
+    param (
+        [Parameter(Mandatory)]$Name
+    )
+    
+    $ApplicationDefinition | 
+    where Name -EQ $Name
+}
+
+function New-TervisTechnicalServicesApplicationVM {
+    Param(
+        [Parameter(Mandatory)]
+        [ValidateSet('SFTP')]
+        $ApplicationDefinitionName
+    )
+    $ApplicationDefinitoin = Get-TervisApplicationDefinition -Name $ApplicationDefinitionName
+    
+
+    $LastComputerNameCountFromAD = (
+        get-adcomputer -filter "name -like '$($ApplicationDefinitoin.ComputerNameSuffixInAD)*'" | 
+        select -ExpandProperty name | 
+        Sort-Object -Descending | 
+        select -last 1
+    ) -replace $ApplicationDefinitoin.ComputernameSuffixInAD,""
+
+    $NextComputerNameWithoutEnvironmentPrefix = "sftp" + ([int]$LastComputerNameCountFromAD + 1).tostring("00")
+    $VM = New-TervisVM -VMNameWithoutEnvironmentPrefix $NextComputerNameWithoutEnvironmentPrefix `
+        -VMSizeName $ApplicationDefinitoin.VMSizeName -VMOperatingSystemTemplateName $ApplicationDefinitoin.VMOperatingSystemTemplateName -EnvironmentName $ApplicationDefinitoin.Environmentname -Cluster $Cluster -DHCPScopeID $DHCPScopeID -Verbose   
+    $TervisVMObject = $vm | get-tervisVM
+    $TervisVMObject
+}
