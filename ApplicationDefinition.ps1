df57df7ef624bf4023ac0406b66255cc0263571e
hambone124 <hpe60m5@gmail.com>
2017-11-01 17:29:05 -0400

    FSM VM +1

--- ApplicationDefinition.ps1
+++ ApplicationDefinition.ps1
@@ -573,7 +573,7 @@
     ComputeType = "Virtual"
     Environments = [PSCustomObject][Ordered]@{
         Name = "Infrastructure"
-        NumberOfNodes = 1
+        NumberOfNodes = 2
         VMSizeName = "Medium"
         LocalAdminPasswordStateID = 5031
     }
