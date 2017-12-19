bd9acd5c23ef8188db74c1ca61e6a36ffe0e763d
Hamilton Perez <hambone124@users.noreply.github.com>
2017-12-19 18:05:09 -0500

    Updated number of EBS RemoteApp servers

--- ApplicationDefinition.ps1
+++ ApplicationDefinition.ps1
@@ -436,7 +436,7 @@
     ComputeType = "Virtual"
     Environments = [PSCustomObject][Ordered]@{
         Name = "Infrastructure"
-        NumberOfNodes = 2
+        NumberOfNodes = 3
         VMSizeName = "Large"
         LocalAdminPasswordStateID = 4351
     }
